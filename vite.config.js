e9c22f16fde3f4b3c1ee0cf24bb801457f2cce39
S7b0t4 <toni.paun.00@mail.ru>
2024-04-21 18:57:04 +0500

    back change

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const postcss = require('rollup-plugin-postcss');
-const postcssUrlRewrite = require('postcss-urlrewrite');
 
 module.exports = {
   root: path.resolve(__dirname, 'src'),
@@ -10,15 +8,4 @@ module.exports = {
   server: {
     port: 8080
   },
-  plugins: [
-    postcss({
-      plugins: [
-        postcssUrlRewrite({
-          rules: [
-            { from: './img/', to: './assets/' }
-          ]
-        })
-      ]
-    })
-  ]
 };
