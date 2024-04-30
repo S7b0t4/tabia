cb437fe20c91d0c1ee812c590b4529c7757c38ad
S7b0t4 <toni.paun.00@mail.ru>
2024-04-30 14:23:52 +0500

    add tabia

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import realEstate from "../Upload/real-estate.jpeg"
 import restoran from "../Upload/restoran.jpeg"
-import stepUp from "../Upload/stepUp.jpg"
+import Tabia from "../Upload/Tabia.png"
 
 import Avatar from "../Upload/avatar.svg"
 
@@ -79,29 +79,28 @@ const DeveloperInfo = () => {
 
 const Portfolio = () => {
 		const projects = [
-			{
-				title: 'Магазин недвижимости',
-				description: 'Разработка веб-платформы для поиска и просмотра недвижимости. Интеграция с картами, фильтрация по параметрам и удобный интерфейс для пользователей.',
-				imageUrl: realEstate,
-				projectLinkGit: 'https://github.com/S7b0t4/real_estate',
-        projectLink:"https://s7b0t4-real-estate.netlify.app"
-
-			},
-			{
-				title: 'Меню для ресторана',
-				description: 'Разработка веб-сайта с простым меню для ресторана. Возможность просмотра блюд, их цен и основной информации. Пользователи могут сделать заказ онлайн.',
-				imageUrl: restoran,
-				projectLinkGit: 'https://github.com/S7b0t4/Restoran',
-        projectLink:"https://s7b0t4-restoran.netlify.app/"
-			},
       {
-				title: 'Магазин по продаже обуви',
-				description: 'Создание онлайн-магазина для продажи обуви. Широкий выбор товаров, удобная навигация и возможность ознакомиться с отзывами покупателей.',
-				imageUrl: stepUp,
-				projectLinkGit: '#',
-        projectLink:"#"
-			},
-		];
+        "title": "Магазин недвижимости",
+        "description": "Разработка веб-платформы для поиска и просмотра недвижимости. Интеграция с картами, фильтрация по параметрам и удобный интерфейс для пользователей.",
+        "imageUrl": realEstate,
+        "projectLinkGit": "https://github.com/S7b0t4/real_estate_backend",
+        "projectLink": "https://real-estate.s7b0t4-website-server.ru/"
+      },
+      {
+        "title": "Парсер расписания и клиент для расписания",
+        "description": "Разработка веб-приложения для удобного просмотра расписания занятий. Возможность сбора и обновления данных о занятиях с использованием парсера.",
+        "imageUrl": restoran,
+        "projectLinkGit": "https://github.com/S7b0t4/schedule_backend",
+        "projectLink": "https://schedule.s7b0t4-website-server.ru/"
+      },
+      {
+        "title": "Лендинг для юридической компании",
+        "description": "Создание лендинга для юридической компании. Предоставление информации о услугах, контактная информация и форма обратной связи для клиентов.",
+        "imageUrl": Tabia,
+        "projectLinkGit": "https://github.com/S7b0t4/tabia.git",
+        "projectLink": "https://tabia.s7b0t4-website-server.ru/"
+      }
+    ];
 	
 
   return (
