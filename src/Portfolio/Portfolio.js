043214634c00fa2e1b3617f093229a43dd57130f
S7b0t4 <toni.paun.00@mail.ru>
2024-05-14 21:28:03 +0500

    some fix lskjdf

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import realEstate from "../Upload/real-estate.jpeg"
-import schedule from "../Upload/Schedule.png"
-import Tabia from "../Upload/Tabia.png"
+import realEstate from "../Upload/real_estate.png"
+import schedule from "../Upload/schedule.png"
+import tabia from "../Upload/tabia.png"
 
 import Avatar from "../Upload/avatar.svg"
 
@@ -10,26 +10,6 @@ import telegram from "../img/telegram.png"
 import github from "../img/github.png"
 
 import 'animate.css';
-
-const ProjectCard = ({ title, description, imageUrl, projectLink, projectLinkGit }) => {
-  return (
-    <div className="project-card animate__fadeInUp">
-      <img src={imageUrl} alt={title} className="project-image" />
-      <div className="project-details">
-        <h3 className="project-title">{title}</h3>
-        <p className="project-description">{description}</p>
-        <div className='link_row'>
-          <a href={projectLink} target="_blank" rel="noopener noreferrer" className="project-link">
-            Посмотреть проект
-          </a>
-          <a href={projectLinkGit} target="_blank" rel="noopener noreferrer" className="project-link-git">
-            Посмотреть проект на github
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 
 
@@ -43,18 +23,18 @@ const DeveloperInfo = () => {
       </div>
       <div className='text_info'>
         <div className='cool_text animate__pulse'>Привет, меня зовут S7b0t4</div>
-        <div>
+        <p>
           Web - developer
-        </div>
-        <div>
-          Entry level (Начального уровня)
-        </div>
-        <div>
-          Владею - Стеком MERN и всём что близко к пониманию
-        </div>
-        <div>
-          Вы можете попросить или заказать у меня сайт Простого/Среднего уровня по ссылкам
-        </div>
+        </p>
+        <p>
+          <span className="language_name">HTML, CSS, JS</span> - более 2 лет
+        </p>
+        <p>
+          <span className="language_name">React, node.js, express, sqlite, mongoDB</span> - более 1,5 года
+        </p>
+        <p>
+          <span className="language_name">TypeScript, tailwind, NEXT.js, bootstrap</span> - более 1 года 
+        </p>
       </div>
       <div className='row_gap'>
         <a href={"https://github.com/S7b0t4"} className='img_link' >
@@ -84,38 +64,61 @@ const Portfolio = () => {
         "description": "Разработка веб-платформы для поиска и просмотра недвижимости. Интеграция с картами, фильтрация по параметрам и удобный интерфейс для пользователей.",
         "imageUrl": realEstate,
         "projectLinkGit": "https://github.com/S7b0t4/real_estate_backend",
-        "projectLink": "https://real-estate.s7b0t4-website-server.ru/"
+        "projectLink": "https://real-estate.s7b0t4-website-server.ru"
+      },
+      {
+        "title": "Лендинг для юридической компании",
+        "description": "Создание лендинга для юридической компании. Предоставление информации о услугах, контактная информация и форма обратной связи для клиентов.",
+        "imageUrl": tabia,
+        "projectLinkGit": "https://github.com/S7b0t4/tabia.git",
+        "projectLink": "https://tabia.s7b0t4-website-server.ru"
       },
       {
         "title": "Парсер расписания и клиент для расписания",
         "description": "Разработка веб-приложения для удобного просмотра расписания занятий. Возможность сбора и обновления данных о занятиях с использованием парсера.",
         "imageUrl": schedule,
         "projectLinkGit": "https://github.com/S7b0t4/schedule_backend",
-        "projectLink": "https://schedule.s7b0t4-website-server.ru/"
-      },
-      {
-        "title": "Лендинг для юридической компании",
-        "description": "Создание лендинга для юридической компании. Предоставление информации о услугах, контактная информация и форма обратной связи для клиентов.",
-        "imageUrl": Tabia,
-        "projectLinkGit": "https://github.com/S7b0t4/tabia.git",
-        "projectLink": "https://tabia.s7b0t4-website-server.ru/"
+        "projectLink": "https://schedule.s7b0t4-website-server.ru"
       }
     ];
+
+    const ProjectCard = ({ title, description, imageUrl, projectLink, projectLinkGit }) => {
+      return (
+        <div className="project-card animate__fadeInUp">
+          <img src={imageUrl} alt={title} className="project-image" />
+          <div className="project-details">
+            <h3 className="project-title">{title}</h3>
+            <a href={projectLink} target="_blank" rel="noopener noreferrer">
+              {projectLink}
+            </a>
+            <p className="project-description">{description}</p>
+            <div className='link_row'>
+              <a href={projectLinkGit} target="_blank" rel="noopener noreferrer" className="project-link-git">
+              GitHub
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+    };
 	
 
   return (
     <div className="portfolio">
       <DeveloperInfo />
-      {projects.map((project, index) => (
-        <ProjectCard
-          key={index}
-          title={project.title}
-          description={project.description}
-          imageUrl={project.imageUrl}
-          projectLink={project.projectLink}
-          projectLinkGit={project.projectLinkGit}
-        />
-      ))}
+      <h1 className="portfolio_title">Работы</h1>
+      <div className="portfolio_row">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            title={project.title}
+            description={project.description}
+            imageUrl={project.imageUrl}
+            projectLink={project.projectLink}
+            projectLinkGit={project.projectLinkGit}
+          />
+        ))}
+      </div>
     </div>
   );
 };
